ae716bfdf4a3b4809494f0286b9e5f4a99b9f196
Benas Brazdziunas <b.brazdziunas@gmail.com>
2021-06-07 19:15:12 +0100

    enable can

--- speeduino/board_stm32_official.h
+++ speeduino/board_stm32_official.h
@@ -313,6 +313,9 @@ void ignitionSchedule8Interrupt(HardwareTimer*);
 #include <src/STM32_CAN/STM32_CAN.h>
 //This activates CAN1 interface on STM32, but it's named as Can0, because that's how Teensy implementation is done
 STM32_CAN Can0 (_CAN1,DEF);
+
+
+#define USE_CAN2 true
 STM32_CAN Can1 (_CAN2,ALT);
 /*
 Second CAN interface is also available if needed or it can be used also as primary CAN interface.
